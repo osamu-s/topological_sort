906cced10bd0025ae1090caf1d67732952a5bc62
osamu-s <osamus@star.odn.ne.jp>
2018-11-30 00:11:18 +0900

    fix bug and update theme

--- kahn2.py
+++ kahn2.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
 
+# change inverse edge matrix to dependency count
+
 edge = [ {1, 3},
          {3},
          {},
          {2} ]
 
 
-def inverse(e):
-    inv_e = [set() for i in e]
-    for i, a in enumerate(e):
+def dep_count(e):
+    dep_mat = [0]*len(e)
+    for a in e:
         for n in a:
-            inv_e[n].add(i)
-    return inv_e
+            dep_mat[n] += 1
+    return dep_mat
 
 
 def khan(e):
-    inv_e = inverse(e)
-    s = [ i for i, n in enumerate(inv_e) if len(n) == 0 ]
+    dep_mat = dep_count(e)
+    s = [ i for i, n in enumerate(dep_mat) if n == 0 ]
     l = list()
 
     while len(s) > 0:
         n = s.pop()
         l.append(n)
         for m in e[n]:
-            inv_e[m].remove(n)
-            if len(inv_e[m]) == 0:
+            dep_mat[m] -= 1
+            if dep_mat[m] == 0:
                 s.append(m)
 
-    if sum([len(e) for e in inv_e]) > 0:
-        print('error', l, s, inv_e)
+    if sum([d for d in dep_mat]) > 0:
+        print('error', l, s, dep_mat)
     else:
         print(l)
 
